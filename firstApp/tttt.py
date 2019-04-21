a21a86fdbbf87bcfb729b063f893e23a22f265d3
katsunanntoka <katsuya.yokoyama@zozo.com>
2019-04-21 23:17:48 +0900

    implementation of Google Calendar API -> almost done

---
+++ firstApp/tttt.py
@@ -0,0 +1,68 @@
+
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client, tools
+from oauth2client.file import Storage
+
+import datetime
+import pickle
+
+
+## たぶんpython3系じゃないと動かない、、？
+import argparse
+
+class GoogleCalendarAPI():
+
+
+    def __init__(self, target_address='primary'):
+
+        # self.target_address = target_address
+        # self.flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+
+
+        # NOTE とりあえず会議室のアドレスに自動的になるようにしてます
+        self.target_address = "zozo.com_343538343931333532@resource.calendar.google.com"
+        with open('/home/katsuya/flags.pickle', 'rb') as f:
+            self.flags = pickle.load(f)
+
+    def get_credentials(self):
+
+        SCOPES = 'https://www.googleapis.com/auth/calendar.readonry'
+        CLIENT_SECRET_FILE = '/home/katsuya/workspace/client_secret.json'
+        APPLICARION_NAME = 'Google Calendar API Python Quickstart'
+
+        home_dir = os.path.expanduser('~')
+        credential_dir = os.path.join(home_dir, '.credentials')
+        if not os.path.exists(credential_dir):
+            os.makedirs(credential_dir)
+        credential_path = os.path.join(credential_dir,
+                                       'calendar-python-quickstart.json')
+
+        store = Storage(credential_path)
+        credentials = store.get()
+        if not credentials or credentials.invalid:
+            flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+            flow.user_agent = APPLICARION_NAME
+            credentials = tools.run_flow(flow, store, self.flags)
+
+        return credentials
+
+    def get_schedules(self):
+
+        credentials = self.get_credentials()
+        http = credentials.authorize(httplib2.Http())
+        service = discovery.build('calendar', 'v3', http=http)
+
+        now = datetime.datetime.utcnow().isoformat() + 'Z' # Z indicates UTC time
+        eventsResult = service.events().list(
+            calendarId=self.target_address, timeMin=now,
+            maxResults=1, singleEvents=True,
+            orderBy='startTime').execute()
+        events = eventsResult.get('items', [])
+
+        if not events:
+            return '何も帰ってきてないよ'
+
+        return events
