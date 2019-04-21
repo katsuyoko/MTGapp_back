6896cc8d843939952002c472372528dac44c96fb
pkino <ikuhide131610@yahoo.co.jp>
2019-04-22 08:49:03 +0900

    get data from calender

--- firstApp/views.py
+++ firstApp/views.py
@@ -22,6 +22,17 @@ from datetime import datetime, timedelta
 from .forms import *
 from .models import Credentials
 
+# from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
 
 class Top(LoginRequiredMixin, generic.TemplateView):
     """トップページ"""
@@ -50,20 +61,44 @@ class Logout(LoginRequiredMixin, LogoutView):
 
 @login_required
 def auth(request):
-    # 既に認証済みならトップページへ
-    if hasattr(request.user, 'credentials'):
-        return redirect('firstApp:top')
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'client_id.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
 
-    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-        settings.CLIENT_SECRET, settings.SCOPES
-    )
-    flow.redirect_uri = settings.REDIRECT_URI
-    authorization_url, state = flow.authorization_url(
-        approval_prompt='force',
-        access_type='offline',
-        include_granted_scopes='true')
-    request.session['state'] = state
-    return redirect(authorization_url)
+    service = build('calendar', 'v3', credentials=creds)
+
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                        maxResults=10, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
+
 
 @login_required
 def callback(request):
