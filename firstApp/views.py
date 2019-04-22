91012ecd611ae779f85da0205ee6f69b09c85b60
katsunanntoka <katsuya.yokoyama@zozo.com>
2019-04-22 09:38:37 +0900

    miss

--- firstApp/views.py
+++ firstApp/views.py
@@ -124,7 +124,7 @@ class GoogleCalendarAPI():
         # NOTE とりあえず会議室のアドレスに自動的になるようにしてます
         self.target_address = "zozo.com_343538343931333532@resource.calendar.google.com"
         home_dir = os.path.expanduser('~')
-        with open(os.path.joint(home_dir, 'flags.pickle'), 'rb') as f:
+        with open(os.path.join(home_dir, 'flags.pickle'), 'rb') as f:
             self.flags = pickle.load(f)
 
     def get_credentials(self):
