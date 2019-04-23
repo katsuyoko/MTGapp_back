474540570bb64bb54e953f18e5189f3fd9601f82
katsunanntoka <katsuya.yokoyama@zozo.com>
2019-04-23 11:41:38 +0900

    minor change

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,6 @@ app_name = 'api'
 
 urlpatterns = [
     path('calendar_info/<str:mail_address>/', views.get_calendar_info, name='calInfo'),
-    path('item_info/<str:price>/', views.get_item_info, name='itemInfo'),
+    path('item_info/<int:price>/', views.get_item_info, name='itemInfo'),
     # path('save/', views.save_info, name='save'),
 ]
