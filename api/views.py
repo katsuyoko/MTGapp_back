6e69a2f89540e9835d8faf682ee18b3365f1638b
hrsma2i <hrs.ma2i@gmail.com>
2019-04-23 11:30:44 +0900

    Change item detail url

--- api/views.py
+++ api/views.py
@@ -88,7 +88,7 @@ def get_item_info(request, price):
     res = requests.get(url)
     soup = BeautifulSoup(res.content)
     items = soup.select("#searchResultList > li")
-    item = items[0]
+    item = items[6]
 
     # 商品の値段
     price = item.find('div', class_="catalog-price-amount").text
@@ -104,9 +104,9 @@ def get_item_info(request, price):
     brand = item.find('div', class_='catalog-h').text
 
     # 商品名
-    rel_url = item.find('a', class_='catalog-link')['href'].lstrip('/')
-    item_detail_url = os.path.join('https://zozo.jp', rel_url)
-    item_detail_url
+    item_detail_url = item.find('a', class_='catalog-link')['href'].lstrip('/')
+    if not item_detail_url.startswith('http'):
+        item_detail_url = os.path.join('https://zozo.jp', item_detail_url)
     res_detail = requests.get(item_detail_url)
     soup_detail = BeautifulSoup(res_detail.content)
     name = soup_detail.find('h1').text
