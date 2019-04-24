7555f449d4e5569f330e58de8de7802ff1f67e9a
katsunanntoka <katsuya.yokoyama@zozo.com>
2019-04-24 10:39:57 +0900

    modify time info

--- api/views.py
+++ api/views.py
@@ -89,8 +89,19 @@ def get_calendar_info(request, mail_address):
 
     for info_dict in gca:
         ## 時間の抽出
-        start = datetime.datetime.strptime(info_dict['start']['dateTime'][:-6], '%Y-%m-%dT%H:%M:%S')
-        end = datetime.datetime.strptime(info_dict['end']['dateTime'][:-6], '%Y-%m-%dT%H:%M:%S')
+        print('#### ', info_dict['start'])
+        if 'dateTime' in info_dict['start'].keys():
+            start = datetime.datetime.strptime(info_dict['start']['dateTime'][:-6], '%Y-%m-%dT%H:%M:%S')
+            end = datetime.datetime.strptime(info_dict['end']['dateTime'][:-6], '%Y-%m-%dT%H:%M:%S')
+            utc_s = info_dict['start']['dateTime']
+            utc_e = info_dict['end']['dateTime']
+
+        else:
+            today = datetime.datetime.today()
+            start = datetime.datetime(today.year, today.month, today.day, 8, 0, 0)
+            end = datetime.datetime(today.year, today.month, today.day, 20, 0, 0)
+            utc_s = start.strftime('%Y-%m-%dT%H:%M:%S+09:00')
+            utc_e = end.strftime('%Y-%m-%dT%H:%M:%S+09:00')
 
         ## プライベート設定への対処
         # NOTE とりあえず、必要なキーのあるなしで返す要素を変更するようにしてます。
@@ -151,8 +162,8 @@ def get_calendar_info(request, mail_address):
 
 
         config = {}
-        config['start'] = {'year':start.year, 'month':start.month, 'day':start.day, 'hour':start.hour, 'minute':start.minute, 'utc':info_dict['start']['dateTime']}
-        config['end'] = {'year':end.year, 'month':end.month, 'day':end.day, 'hour':end.hour, 'minute':end.minute, 'utc':info_dict['end']['dateTime']}
+        config['start'] = {'year':start.year, 'month':start.month, 'day':start.day, 'hour':start.hour, 'minute':start.minute, 'utc':utc_s}
+        config['end'] = {'year':end.year, 'month':end.month, 'day':end.day, 'hour':end.hour, 'minute':end.minute, 'utc':utc_e}
         config['title'] = title
         config['attendees'] = {
                 'num': num_attendees,
